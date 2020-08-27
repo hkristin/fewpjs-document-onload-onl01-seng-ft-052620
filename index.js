7e3a657bd8bb8c3c69800bda9545c502f0b1b7a9
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-27 23:22:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+});
+
+function updateDOM() {
+  document.getElementById( "text" )
+    .innerHTML = "Hello!";
+}
